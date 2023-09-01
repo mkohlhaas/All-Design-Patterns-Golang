e5153af103aab2c9b05098fb8dcdf8eb39c23072
Hans-J. Schmid <knock@myopendoor.de>
2023-09-02 01:09:26 +0200

    adds some clarifying comments to visitor pattern.

--- Behavioural-Design-Patterns/Visitor/main.go
+++ Behavioural-Design-Patterns/Visitor/main.go
@@ -5,10 +5,42 @@ import (
 	"math"
 )
 
+// Separation of data and algorithms:
+// Both are interfaces:
+// data = shape
+// processing = visitor
+
+// Golang can accept interfaces as functions params,
+// but it can't be the receiver of a method.
+
+// Does not work (uncoment to see for yourself):
+// func (i *interface{}) foo() {}
+
+// data accepts visitors.
+// visitors visit data.
+
+// SO -> OS
+// subject becomes object and object becomes subject
+
+// `accept` and `visit` are symmetrical mirror images.
+// Functions (processing) are data and vice versa.
+// But the symmetry is broken by the data calling the very specific functions:
+// - visitForSquare
+// - visitForCircle
+// - visitForRectangle
+
 //////////// Interface for Shapes ////////////////
 
 type shape interface {
 	accept(visitor)
+}
+
+//////////// Visitor Interface ///////////////////
+
+type visitor interface {
+	visitForSquare(*square)
+	visitForCircle(*circle)
+	visitForRectangle(*rectangle)
 }
 
 //////////////// Square //////////////////////////
@@ -17,6 +49,9 @@ type square struct {
 	side float64
 }
 
+// The shape knows which visit method to call!!!
+// subject square becomes object (func param) and
+// object visitor becomes subject.
 func (s *square) accept(v visitor) {
 	v.visitForSquare(s)
 }
@@ -31,6 +66,9 @@ type circle struct {
 	radius float64
 }
 
+// The shape knows which visit method to call!!!
+// subject circle becomes object (func param) and
+// object visitor becomes subject.
 func (c *circle) accept(v visitor) {
 	v.visitForCircle(c)
 }
@@ -46,20 +84,15 @@ type rectangle struct {
 	b float64
 }
 
+// The shape knows which visit method to call!!!
+// subject rectangle becomes object (func param) and
+// object visitor becomes subject.
 func (t *rectangle) accept(v visitor) {
 	v.visitForRectangle(t)
 }
 
 func (t *rectangle) String() string {
 	return "Rectangle"
-}
-
-//////////// Visitor Interface ///////////////////
-
-type visitor interface {
-	visitForSquare(*square)
-	visitForCircle(*circle)
-	visitForRectangle(*rectangle)
 }
 
 ////////////// Area Visitor //////////////////////
@@ -83,6 +116,12 @@ func (a *areaCalculator) visitForCircle(c *circle) {
 func (a *areaCalculator) visitForRectangle(r *rectangle) {
 	a.area = r.b * r.l
 	fmt.Printf("Area for %s: %0.2f\n", r, a.area)
+}
+
+// subject visitor becomes object (func param) and
+// object shape becomes subject.
+func (v *areaCalculator) visit(s shape) {
+	s.accept(v)
 }
 
 ////// Middle Coordinates Visitor ////////////////
@@ -112,6 +151,11 @@ func (mc *middleCoordinates) visitForRectangle(r *rectangle) {
 	fmt.Printf("Middle point coordinates for %s: x = %0.2f, y = %0.2f\n", r, mc.x, mc.y)
 }
 
+// object shape becomes subject.
+func (v *middleCoordinates) visit(s shape) {
+	s.accept(v)
+}
+
 ////////////// Main //////////////////////////////
 
 func main() {
@@ -120,28 +164,46 @@ func main() {
 	circle := &circle{radius: 3}
 	rectangle := &rectangle{l: 2, b: 3}
 
-	// create area calculator
+	// create calculators
 	areaCalculator := &areaCalculator{}
+	mcCalculator := &middleCoordinates{}
 
 	fmt.Println("Calculate areas:")
 	square.accept(areaCalculator)
 	circle.accept(areaCalculator)
 	rectangle.accept(areaCalculator)
+	fmt.Println("\nThe same but idiomatic.")
+	areaCalculator.visit(square)
+	areaCalculator.visit(circle)
+	areaCalculator.visit(rectangle)
 
-	fmt.Println("\nCreate middle coordinates calculator:")
-	mcCalculator := &middleCoordinates{}
+	fmt.Println("\nCalculate middle points:")
 	square.accept(mcCalculator)
 	circle.accept(mcCalculator)
 	rectangle.accept(mcCalculator)
+	fmt.Println("\nThe same but idiomatic.")
+	mcCalculator.visit(square)
+	mcCalculator.visit(circle)
+	mcCalculator.visit(rectangle)
 
+	// areaCalculator
 	fmt.Println("\nLooping over shapes:")
 	shapes := []shape{square, circle, rectangle}
 	for _, shape := range shapes {
 		shape.accept(areaCalculator)
 	}
+	fmt.Println("\nThe same but idiomatic.")
+	for _, shape := range shapes {
+		areaCalculator.visit(shape)
+	}
 
+	// middleCoordinates
 	fmt.Println("\nLooping over shapes:")
 	for _, shape := range shapes {
 		shape.accept(mcCalculator)
+	}
+	fmt.Println("\nThe same but idiomatic.")
+	for _, shape := range shapes {
+		mcCalculator.visit(shape)
 	}
 }
